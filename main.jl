21070d61464d99d5138b1d47497afda7c4a2d1b8
jsschuler <jsschuler@gmail.com>
2024-06-20 09:04:58 -0400

    modified:   main.jl

--- main.jl
+++ main.jl
@@ -21,7 +21,7 @@ include("functions.jl")
 
 ctrlFile=ARGS[1]
 #println(ctrlFile)
-ctrlFrame=load_object(ctrlFile)
+ctrlFrame=@load ctrlFile
 #println(ctrlFrame[1:10,:])
 # load only incomplete models
 ctrlWorking=ctrlFrame[ctrlFrame[:,"complete"].==false,:]
