797f6999af1640b555e1ae794f5b88a42b1a6619
jsschuler <jsschuler@gmail.com>
2025-02-21 12:03:05 -0500

    modified:   functions.jl 	modified:   main0001.jl

--- functions.jl
+++ functions.jl
@@ -453,10 +453,14 @@ function optimFuncGen(insur::Float64,prod::Float64,objP::Float64,riskAversion::F
                        outFrame.jointProbObj .* log2.(outFrame.jointProbObj./outFrame.M)
         println(outFrame)
         println("Divergence for parameter")
-        println("subjective P: "*string(params[:subjP]))
+        println("subjective P "*string(params[:subjP]))
         println("Objective P: "*string(objP))
         println(sum(outFrame.JSDiv))
-        return sum(outFrame.JSDiv)
+        if params[:subjP]==0.0
+            return 10000.0
+        else
+            return sum(outFrame.JSDiv)
+        end
     end
     return runInstances
 end
@@ -467,7 +471,7 @@ end
 function optimize(insur::Float64,prod::Float64,objP::Float64,riskAversion::Float64)
     optFunc=optimFuncGen(insur,prod,objP,riskAversion)
     space = Dict(
-    :subjP => HP.QuantUniform(:subjP,0.01,.01, 1.0)
+    :subjP => HP.QuantUniform(:subjP,0.0,.01, 1.0)
     )
 
     best = fmin(
--- main0001.jl
+++ main0001.jl
@@ -24,7 +24,7 @@ tuples=[]
 
 for insur in 0.5:0.1:0.6
     for prod in 0.5:0.1:0.6
-        for objP in 0.2:0.2:1.0
+        for objP in 0.0:0.2:1.0
             push!(tuples,(insur,prod,objP))
         end
     end
