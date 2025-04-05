a2c5308f58950de6a90ddd0a7f87eeb66575f21e
jsschuler <jsschuler@gmail.com>
2025-04-05 18:04:37 -0400

    modified:   main0001.jl

--- main0001.jl
+++ main0001.jl
@@ -19,50 +19,49 @@ using CSV
 cores=Sys.CPU_THREADS
 
 #println(runFamily(.5,.5,.2))
-# now, generate the possible values of insurance and production
+ now, generate the possible values of insurance and production
+tuples=[]
 
-#tuples=[]
-#
-#for insur in 0.5:0.1:0.6
-#    for prod in 0.5:0.1:0.6
-#        for objP in 0.0:0.2:1.0
-#            push!(tuples,(insur,prod,objP))
-#        end
-#    end
-#end
-#
-## count completed processes
-#procCnt=length(tuples)
-#doneCnt=0
-#coreDict=Dict()
-#for coreNum in 2:cores
-#    coreDict[coreNum] = nothing
-#end
-#
-## testing 
-#
-#
-## now we run the process
-#while doneCnt < procCnt
-#    for c in keys(coreDict)
-#        # if the core contains nothing, send it an optimization procedure
-#        if isnothing(coreDict[c]) && length(tuples) > 0
-#            currTup=popfirst!(tuples)
-#            println("Sending tuple "*string(currTup)*" to core "*string(c))
-#            println(length(tuples))
-#            coreDict[c]=@spawnat c runFamily(currTup[1],currTup[2],currTup[3])
-#        elseif isReady(coreDict[c])
-#            result=fetch(coreDict[c])
-#            global doneCnt
-#            doneCnt=doneCnt+1
-#            coreDict[c]=nothing
-#            df=DataFrame(
-#                        insur=result[2],
-#                        prod=result[3],
-#                        objP=result[4]
-#                        failProb=result[1])
-#            println("Writing File")
-#            CSV.write("../data/results.csv", df,header = false,append=true)
-#        end
-#    end
-#end
+for insur in 0.5:0.1:0.6
+    for prod in 0.5:0.1:0.6
+        for objP in 0.0:0.2:1.0
+            push!(tuples,(insur,prod,objP))
+        end
+    end
+end
+
+# count completed processes
+procCnt=length(tuples)
+doneCnt=0
+coreDict=Dict()
+for coreNum in 2:cores
+    coreDict[coreNum] = nothing
+end
+
+# testing 
+
+
+# now we run the process
+while doneCnt < procCnt
+    for c in keys(coreDict)
+        # if the core contains nothing, send it an optimization procedure
+        if isnothing(coreDict[c]) && length(tuples) > 0
+            currTup=popfirst!(tuples)
+            println("Sending tuple "*string(currTup)*" to core "*string(c))
+            println(length(tuples))
+            coreDict[c]=@spawnat c runFamily(currTup[1],currTup[2],currTup[3])
+        elseif isReady(coreDict[c])
+            result=fetch(coreDict[c])
+            global doneCnt
+            doneCnt=doneCnt+1
+            coreDict[c]=nothing
+            df=DataFrame(
+                        insur=result[2],
+                        prod=result[3],
+                        objP=result[4]
+                        failProb=result[1])
+            println("Writing File")
+            CSV.write("../data/results.csv", df,header = false,append=true)
+        end
+    end
+end
